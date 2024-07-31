301d74c20d9d83c62ca438879783a253f01fc7ce
samhatter <sammyvincent2001@gmail.com>
2024-07-30 17:39:25 -0700

    did create_tree

--- src/main.py
+++ src/main.py
@@ -1,28 +1,44 @@
-# tree: {"func":func, "children":[...]}
-# size: {"height":height, "width":width}
-# ascii_map: [{"threshold":thresh0, "character":char0}, {"threshold":thresh1, "character":char1}]
-# func_dict: {"1":[{"prob":prob0, "func":func0}]}
-# coords: {"x":x, "y":y}
-
 import random
+from typing import List
+from typing import Tuple
 
-def create_tree(func_dict, max_depth):
+class Func:
+    def __init__(self, func: function, prob: int, args:int):
+        self.func = func
+        self.args = args
+        self.prob = prob
+        
+class Node:
+    def __init__(self, func: function, children: List['Node']):
+        self.func = func
+        self.children = children
+
+def create_tree(func_list: List[Func], max_depth) -> Node:
     if max_depth == 1:
-        avail_funcs = func_dict["2"]
-        rand = random.uniform(0, sum([func["prob"] for func in avail_funcs]))
-        func = max(avail_funcs, key=lambda func: func["prob"] if func["prob"] < )
-        return {}
+        avail_funcs = filter(lambda func: func.args == 2, func_list)
+        rand_num = random.randint(0, sum(map(lambda func: func.prob, avail_funcs))-1)
+        accumulator = 0
+        for func in avail_funcs:
+            accumulator += func.prob
+            if rand_num < accumulator:
+                rand_func = func
+            return Node(rand_func.func, [])
     else:
-        rand = random.uniform(0, 1)
-        return create_tree(func_dict_max_depth-1)
+        rand_num = random.randint(0, sum(map(lambda func: func.prob, func_list))-1)
+        accumulator = 0
+        for func in func_list:
+            accumulator += func.prob
+            if rand_num < accumulator:
+                rand_func = func
+                return Node(rand_func.func, [create_tree(func_list,max_depth-1)])
 
-def eval_tree_at_pixel(tree, coords, ascii_map):
-    return
+def eval_tree_at_pixel(node: Node, coords: Tuple, ascii_map):
+    
 
 def eval_tree_over_grid(tree, size, ascii_map):
     return
 
-def main(func_dict, size, ascii_map, max_depth):
+def main(func_list, size, ascii_map, max_depth):
     return
 
 if __name__ == "__main__":
